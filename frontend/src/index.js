6cc3d572e8d438e03b21c1d466909fb29d60cbdf
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-25 16:19:36 +0100

    Added Auth0 provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,23 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+console.log(domain, clientId);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
