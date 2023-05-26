2c84243fbac3aa4adc4875edd5fe3e4be985227e
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-26 11:53:30 +0100

    Added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
 import "./App.css";
 import Navbar from "./layout/Navbar";
 import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Articles from "./pages/Articles";
 
 function App() {
   const { isLoading, error } = useAuth0();
   return (
     <div className="App">
-      <Navbar />
-      {error && <p>Authentication Error</p>}
-      {!error && isLoading && <p>Loading...</p>}
-      {!error && !isLoading && (
-        <>
-          <Home />
-        </>
-      )}
+      <BrowserRouter>
+        <Navbar />
+        {error && <p>Authentication Error</p>}
+        {!error && isLoading && <p>Loading...</p>}
+        {!error && !isLoading && (
+          <>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/articles" element={<Articles />} />
+            </Routes>
+          </>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
