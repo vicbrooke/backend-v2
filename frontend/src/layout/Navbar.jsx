f202c32659702c7eedb6aa34df8d7367f4048c51
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-26 11:54:26 +0100

    Added links Added classNames

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import LoginButton from "../components/LoginButton";
 import LogoutButton from "../components/LogoutButton";
 import Profile from "../components/Profile";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <section>
-      <LoginButton />
-      <LogoutButton />
-      <Profile />
+    <section className="navbar">
+      <div className="navbar-content">
+        <div className="login">
+          <LoginButton />
+          <LogoutButton />
+        </div>
+        <div className="profile">
+          <Profile />
+        </div>
+      </div>
+      <h1>My App</h1>
+      <div className="links">
+        <Link to={"/"}>Home Page</Link>
+        <Link to={"/articles"}>View Articles</Link>
+      </div>
     </section>
   );
 }
