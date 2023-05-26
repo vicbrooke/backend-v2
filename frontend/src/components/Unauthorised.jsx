6341103e1785dd5b15f2d450c12ac544e89f301d
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-26 13:18:06 +0100

    Added Unauthorised message component

---
+++ frontend/src/components/Unauthorised.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Unauthorised() {
+  return (
+    <section>
+      <h2>You must be logged in to view this page</h2>
+    </section>
+  );
+}
+
+export default Unauthorised;
--- frontend/src/pages/Articles.jsx
+++ frontend/src/pages/Articles.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import { useAuth0 } from "@auth0/auth0-react";
+import Unauthorised from "../components/Unauthorised";
 
 function Articles() {
   const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
@@ -26,26 +27,24 @@ function Articles() {
     fetchData();
   }, [getAccessTokenSilently]);
 
-  return (
-    isAuthenticated &&
-    !isLoading &&
-    articles && (
-      <section className="articles">
-        {console.log(articles)}
-        {articles.map((article) => {
-          return (
-            <article>
-              <h2>{article.title}</h2>
-              <p>{article.body}</p>
-              <div className="article-info">
-                <p>Author: {article.user.name}</p>
-                <p>Comments: {article.comments.length}</p>
-              </div>
-            </article>
-          );
-        })}
-      </section>
-    )
+  return isAuthenticated && !isLoading && articles ? (
+    <section className="articles">
+      {console.log(articles)}
+      {articles.map((article) => {
+        return (
+          <article>
+            <h2>{article.title}</h2>
+            <p>{article.body}</p>
+            <div className="article-info">
+              <p>Author: {article.user.name}</p>
+              <p>Comments: {article.comments.length}</p>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  ) : (
+    <Unauthorised />
   );
 }
 
