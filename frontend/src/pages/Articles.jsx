576e4afb715d681d927c405ac5dc5c29875ca4e4
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-27 12:20:42 +0100

    Added pagination Added ability to alter number of articles displayed

--- frontend/src/pages/Articles.jsx
+++ frontend/src/pages/Articles.jsx
@@ -3,45 +3,109 @@ import axios from "axios";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import Unauthorised from "../components/Unauthorised";
+// import Pagination from "../components/Pagination";
 
 function Articles() {
   const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
   const [articles, setArticles] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = await getAccessTokenSilently();
-        console.log(token);
-        const response = await axios.get("http://localhost:4000/articles", {
+  const [articlesPerPage, setArticlesPerPage] = useState(10);
+
+  const fetchData = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      console.log(token);
+      const response = await axios.get(
+        `http://localhost:4000/articles?limit=${articlesPerPage}&page=${currentPage}`,
+        {
           headers: {
             authorization: `Bearer ${token}`,
           },
-        });
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+        }
+      );
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
-  }, [getAccessTokenSilently]);
+  }, [articlesPerPage]);
+
+  const handleClick = async () => {
+    fetchData();
+  };
 
   return isAuthenticated && !isLoading && articles ? (
-    <section className="articles">
-      {console.log(articles)}
-      {articles.map((article) => {
-        return (
-          <article>
-            <h2>{article.title}</h2>
-            <p>{article.body}</p>
-            <div className="article-info">
-              <p>Author: {article.user.name}</p>
-              <p>Comments: {article.comments.length}</p>
-            </div>
-          </article>
-        );
-      })}
+    <section>
+      <div className="pagination">
+        <label>
+          Results per page:
+          <select
+            onChange={(event) => {
+              setArticlesPerPage(Number(event.target.value));
+            }}
+          >
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+            <option value={30}>30</option>
+            <option value={40}>40</option>
+          </select>
+        </label>
+        <button
+          onClick={() => {
+            setCurrentPage(currentPage - 1);
+            handleClick();
+          }}
+        >
+          Previous
+        </button>
+
+        <button
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+            handleClick();
+          }}
+        >
+          Next
+        </button>
+      </div>
+      <div className="articles">
+        {console.log(articles)}
+        {articles.map((article) => {
+          return (
+            <article>
+              <h2>{article.title}</h2>
+              <p>{article.body}</p>
+              <div className="article-info">
+                <p>Author: {article.user.name}</p>
+                <p>Comments: {article.comments.length}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+      <div className="pagination">
+        <button
+          onClick={() => {
+            setCurrentPage(currentPage - 1);
+            handleClick();
+          }}
+        >
+          Previous
+        </button>
+
+        <button
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+            handleClick();
+          }}
+        >
+          Next
+        </button>
+      </div>
     </section>
   ) : (
     <Unauthorised />
